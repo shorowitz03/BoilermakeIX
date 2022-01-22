d213a41c897337dc886bf48aef03ec9d1021db1c
shoro <shorowitz03@gmail.com>
2022-01-22 14:01:28 -0500

    this is big balls

--- sam/TextMessenger.py
+++ sam/TextMessenger.py
@@ -33,49 +33,6 @@ def send_sms_via_email(
         email.login(sender_email, email_password)
         email.sendmail(sender_email, receiver_email, email_message)
 
-def send_mms_via_email(
-    number: str,
-    message: str,
-    file_path: str,
-    mime_maintype: str,
-    mime_subtype: str,
-    provider: str,
-    sender_credentials: tuple,
-    subject: str = "sent using etext",
-    smtp_server: str = "smtp.gmail.com",
-    smtp_port: int = 465,
-):
-
-    sender_email, email_password = sender_credentials
-    receiver_email = f'{number}@{PROVIDERS.get(provider).get("sms")}'
-
-    email_message=MIMEMultipart()
-    email_message["Subject"] = subject
-    email_message["From"] = sender_email
-    email_message["To"] = receiver_email
-
-    email_message.attach(MIMEText(message, "plain"))
-
-    with open(file_path, "rb") as attachment:
-        part = MIMEBase(mime_maintype, mime_subtype)
-        part.set_payload(attachment.read())
-
-        encoders.encode_base64(part)
-        part.add_header(
-            "Content-Disposition",
-            f"attachment; filename={basename(file_path)}",
-        )
-
-        email_message.attach(part)
-
-    text = email_message.as_string()
-
-    with smtplib.SMTP_SSL(
-        smtp_server, smtp_port, context=ssl.create_default_context()
-    ) as email:
-        email.login(sender_email, email_password)
-        email.sendmail(sender_email, receiver_email, text)
-
 
 def main():
     number = global_number
@@ -86,23 +43,56 @@ def main():
 
     # 
     send_sms_via_email(number, message, provider, sender_credentials)
-
     # MMS
-    file_path = "/path/to/file/file.png"
 
-    mime_maintype = "image"
-    mime_subtype = "png"
-    print("Success!")
-#    send_mms_via_email(
-#        number,
-#        message,
-#        file_path,
-#        mime_maintype,
-#        mime_subtype,
-#        provider,
-#        sender_credentials,
-#    )
+    print("Success!")\
 
 
 if __name__ == "__main__":
     main()
+
+########################################################################
+
+#def send_mms_via_email(
+#    number: str,
+#    message: str,
+#    file_path: str,
+#    mime_maintype: str,
+#    mime_subtype: str,
+#    provider: str,
+##    sender_credentials: tuple,
+#    subject: str = "sent using etext",
+#    smtp_server: str = "smtp.gmail.com",
+#    smtp_port: int = 465,
+#):
+#
+#    sender_email, email_password = sender_credentials
+#    receiver_email = f'{number}@{PROVIDERS.get(provider).get("sms")}'##
+
+#    email_message=MIMEMultipart()
+#    email_message["Subject"] = subject
+#    email_message["From"] = sender_email
+#    email_message["To"] = receiver_email#
+#
+ #   email_message.attach(MIMEText(message, "plain"))
+#
+ #   with open(file_path, "rb") as attachment:
+  #      part = MIMEBase(mime_maintype, mime_subtype)
+   #     part.set_payload(attachment.read())
+#
+ #       encoders.encode_base64(part)
+  #      part.add_header(
+   #         "Content-Disposition",
+    #        f"attachment; filename={basename(file_path)}",
+     #   )
+
+ #       email_message.attach(part)
+
+#    text = email_message.as_string()
+
+    #with smtplib.SMTP_SSL(
+   #     smtp_server, smtp_port, context=ssl.create_default_context()
+  #  ) as email:
+ #       email.login(sender_email, email_password)
+#        email.sendmail(sender_email, receiver_email, text)
+
