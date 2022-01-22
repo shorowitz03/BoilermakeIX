1121c3848a889e5f13ef23d13b9963ed87196c62
garrett h <hurstg02@gmail.com>
2022-01-22 10:17:49 -0500

    updated data collection scripts

--- garrett/generateTickers.py
+++ garrett/generateTickers.py
@@ -1,8 +1,17 @@
 import sys 
+import numpy as np
+import pandas as pd
+import yfinance as yf
 
 stocks = [x.split(',')[0].strip(' ') for x in open('garrett\\nasdaq_screener_1642839777222.txt', 'r')]
 
-sys.stdout = open('garrett/tickers.txt', 'w')
-
-for x in stocks:
-    print(x, end=',')
+file = open('garrett/tickers.txt', 'w')
+    
+for s in stocks:
+    try:
+        data = yf.download(tickers=s, period='2m', interval='1m', progress=False)
+        file.write(s + ',')
+        print(data)
+        print(s + ' added')
+    except:
+        print(s + ' not found')
--- garrett/pullTopStock.py
+++ garrett/pullTopStock.py
@@ -3,19 +3,21 @@ import numpy as np
 import pandas as pd
 import yfinance as yf
 
-stocks = ['UBER']
-stocks = [x.split(',')[0].strip(' ', '') for x in open('garrett\\nasdaq_screener_1642839777222.txt', 'r')]
-sys.stdout = open('tickers.txt', 'w')
-for x in stocks:
-    print(x, end=',')
+stocks = open('garrett\\tickers.txt', 'r').readline().split(',')[1:-1]
+print(stocks)
 
+incTickers, excTickers= [], []
 
+#data = yf.download(tickers=stocks[0], start='2020-01-01', interval='1d', progress=False)
+for s in stocks:
+    try:
+        data = yf.download(tickers=s, period='2m', interval='1m', progress=False)
+        incTickers.append(s)
+    except:
+        excTickers.append(s)
 
 '''
-data = yf.download(tickers=stocks[0], start='2020-01-01', interval='1d', progress=False)
-
 fileName = 'stockData/' + stocks[0] + '.txt'
 sys.stdout = open(fileName, 'w')
-
 print(data.to_csv(index=True, line_terminator='\n'))
 '''
