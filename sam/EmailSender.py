febe7caf09e7bc3e7b08337cc33ecd83ace424b2
Bravoeco03 <bernardobbcm@gmail.com>
2022-01-22 15:45:24 -0500

    commit

--- sam/EmailSender.py
+++ sam/EmailSender.py
@@ -4,8 +4,8 @@ from email.message import EmailMessage
 
 subject = "Email From Python"
 body = "This is a test email from python!"
-sender_email = "fluxcache@gmail.com"
-receiver_email = "fluxcache@gmail.com"
+sender_email = "bernardobbcm@gmail.com"
+receiver_email = "bernardobbcm@gmail.com"
 password = input("Enter a password: ")
 
 message = EmailMessage()
