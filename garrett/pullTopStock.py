629d3f6b38f5ad353d45fb227f5b5b6174d3ab56
garrett h <hurstg02@gmail.com>
2022-01-22 11:56:48 -0500

    updated data pulling

--- garrett/pullTopStock.py
+++ garrett/pullTopStock.py
@@ -1,23 +1,38 @@
 import sys
-import numpy as np
-import pandas as pd
 import yfinance as yf
+import pandas as pd
+import pandas as pd
+import numpy as np
+import hvplot.pandas
 
-stocks = open('garrett\\tickers.txt', 'r').readline().split(',')[1:-1]
-print(stocks)
+import mplcyberpunk
+from matplotlib import style
+style.use('cyberpunk')
 
-incTickers, excTickers= [], []
+import matplotlib.pyplot as plt
+import seaborn
+# import tensorflow as tf
 
-#data = yf.download(tickers=stocks[0], start='2020-01-01', interval='1d', progress=False)
-for s in stocks:
-    try:
-        data = yf.download(tickers=s, period='2m', interval='1m', progress=False)
-        incTickers.append(s)
-    except:
-        excTickers.append(s)
+startDate = '2020-01-01'
+''' gets data and plots it
+tkrList = ['GOOG', 'AAPL', 'FB', 'TSLA']
+data = yf.download(tkrList, startDate)['Adj Close']          #gets the data of the stock prices each day
+dailyReturnData = round(data[tkrList].pct_change() * 100, 2)    #gets the percentage return values each day
+print(dailyReturnData.tail(10))
 
+((data.pct_change() + 1).cumprod()).plot()
+plt.legend()
+plt.title("Stock Daily Returns", fontsize=16)
+plt.ylabel('Cumulative Stock Returns', fontsize=14)
+plt.xlabel('Year', fontsize=14)
+
+plt.grid(which="major", color='k', linestyle='-.', linewidth=0.5)
+plt.show()
 '''
-fileName = 'stockData/' + stocks[0] + '.txt'
-sys.stdout = open(fileName, 'w')
-print(data.to_csv(index=True, line_terminator='\n'))
-'''
+
+sp_500_tickers = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')[0]
+sp_500_yf_df = yf.download(sp_500_tickers.Symbol.to_list(), startDate, auto_adjust=True)['Close']
+
+
+print(sp_500_yf_df.tail())
+
