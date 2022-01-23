7e796dac303aed154795fb5e3f3624e9b49b24c7
Bravoeco03 <bernardobbcm@gmail.com>
2022-01-22 21:54:24 -0500

    commit

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,18 +1,8 @@
-"""mysite URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+Use urls as paths (ends) and call functions in views.py
+accordingly.
+"""
+
 from django.contrib import admin
 from django.urls import path
 from . import views
@@ -20,18 +10,28 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+
+    # Home paths
     path('', views.index),
     path('index.html/', views.index),
     path('index.html/template.html/', views.tesla),
+
     path('admin/', admin.site.urls),
-    path('about_us.html/', views.about_us),
     path('products/', views.viewpara, name = 'view_products'),
     path('products/success/', views.success),
-    path('template.html/', views.tesla),
 
-    # Set bar pages
+    # Set market trends paths
     path('MarketTrends.html', views.trends),
-    # path('informational.html', views.informational),
+    path('MarketTrends.html/template.html/', views.tesla),
+
+    # Contact Us paths
     path('contact.html', views.contacts),
-    # path('panel.html', views.opener),
+    path('contact.html/template.html/', views.tesla),
+
+    # About us paths
+    path('about_us.html/', views.about_us),
+    path('about_us.html/template.html/', views.tesla),
+
+     # Stock paths
+    path('template.html/', views.tesla),
 ]
--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -4,41 +4,31 @@ import cgi, cgitb
 
 form = cgi.FieldStorage()
 
-# {{ variable }} The value of a variable is displayed when the variable name 
-# is used inside of double curly braces. This is a form of interpolation.
-# {% tag %} Template tags are enclosed in curly braces with percent signs and are 
-# used for for looping, if else constructs, structural elements, as well as some control logic.
-# {{ variable|filter }} In Django templates, a variable can also have a pipe character 
-# after it to use a template filter. Template filters take a string as input and return a string as output.
-
 # Each function is a page that must be called in urls.py with 
 # views.name with "modifier/"
 
-#BALLZ
+# Bar functions
 def index(request):
-   return render(request, 'index.html') # Third variable can be 
-
-def tesla(request):
-    return render(request, 'template.html')
+   return render(request, 'index.html') # Third variable can be data
 
 def trends(request):
     return render(request, 'MarketTrends.html')
 
-# def informatonal(request):
-#     return render(request, 'opener.html')
+def about_us(request):
+    return render(request, 'about_us.html')
 
 def contacts(request):
     return render(request, 'contact.html')
 
-# def panel(request):
-#     return render(request, 'panel.html')
+
+# Stock functions
+
+def tesla(request):
+    return render(request, 'template.html')
 
 
 
 
-
-def about_us(request):
-    return render(request, 'about_us.html')
 
 def viewpara(request):
     current_user = request.user.get_username()
